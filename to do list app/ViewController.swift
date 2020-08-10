d74329f80894dcb45a47ee98363b35475f6bbcac
Grace Chang <grace.chang313@gmail.com>
2020-08-10 13:10:27 -0700

    Initial Commit

---
+++ to do list app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  to do list app
+//
+//  Created by Grace Chang on 8/10/20.
+//  Copyright © 2020 Grace Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
